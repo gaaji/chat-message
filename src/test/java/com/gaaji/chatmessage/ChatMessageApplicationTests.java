6ef578ce9ba016178f4fcab0d38b4dc7a8149fa4
Seungho <hsh47607@naver.com>
2023-01-11 17:39:05 +0900

    first commit

---
+++ src/test/java/com/gaaji/chatmessage/ChatMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaaji.chatmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
