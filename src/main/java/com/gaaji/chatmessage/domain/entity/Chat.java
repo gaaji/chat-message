e3239bd5ebfae39be013211cfd6a857708509213
Seungho <hsh47607@naver.com>
2023-01-13 22:38:01 +0900

    Add: Chat.java - id 필드를 Bson의 ObjectId 사용 - createdAt 필드 추가 - to entity from request 메소드 추가 - equals and hashcode 커스텀 추가 - mongodb 엔티티로 설정

--- src/main/java/com/gaaji/chatmessage/domain/entity/Chat.java
+++ src/main/java/com/gaaji/chatmessage/domain/entity/Chat.java
@@ -1,8 +1,43 @@
 package com.gaaji.chatmessage.domain.entity;
 
+import com.gaaji.chatmessage.domain.controller.dto.ChatRequest;
+import lombok.Builder;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Data
+@Builder
+@Document(collection = "chats")
 public class Chat {
-    private String id;
+    private ObjectId id;
     private String roomId;
     private String senderId;
     private String content;
+    private Date createdAt;
+
+    public static Chat of(ChatRequest chatDto) {
+        return Chat.builder()
+                .id(ObjectId.get())
+                .roomId(chatDto.getRoomId())
+                .senderId(chatDto.getSenderId())
+                .content(chatDto.getContent())
+                .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Chat c = (Chat) o;
+        return Objects.equals(this.id, c.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
 }
