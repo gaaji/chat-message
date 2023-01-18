a7b8c1307255d07e4d6e7711833c01ce6bee68b7
Seungho <hsh47607@naver.com>
2023-01-18 16:55:08 +0900

    docs: ErrorCode's Code 변경
    - 기존 int 타입의 code에서 String 타입으로 변경
    - 네이밍: C-xxx -> C:Chat Service

--- src/main/java/com/gaaji/chatmessage/global/exception/ErrorCode.java
+++ src/main/java/com/gaaji/chatmessage/global/exception/ErrorCode.java
@@ -1,18 +1,18 @@
 package com.gaaji.chatmessage.global.exception;
 
 public enum ErrorCode {
-    TOKEN_NULL_ERROR(401, "인증 토큰이 없습니다."),
-    INVALIDATED_TOKEN_ERROR(401, "토큰 검증에 실패하였습니다."),
-    MALFORMED_TOKEN_ERROR(401, "토큰의 형식이 유효하지 않습니다."),
-    TOKEN_EXPIRATION_ERROR(401, "인증 토큰이 만료되었습니다."),
-    INVALIDATED_TOKEN_PREFIX_ERROR(401, "토큰 타입이 유효하지 않습니다."),
+    TOKEN_NULL_ERROR("C-101", "인증 토큰이 없습니다."),
+    INVALIDATED_TOKEN_ERROR("C-102", "토큰 검증에 실패하였습니다."),
+    MALFORMED_TOKEN_ERROR("C-103", "토큰의 형식이 유효하지 않습니다."),
+    TOKEN_EXPIRATION_ERROR("C-104", "인증 토큰이 만료되었습니다."),
+    INVALIDATED_TOKEN_PREFIX_ERROR("C-105", "토큰 타입이 유효하지 않습니다."),
     ;
-    private int code;
+    private String code;
     private String message;
-    ErrorCode(int code, String message) {
+    ErrorCode(String code, String message) {
         this.code = code;
         this.message = message;
     }
-    public int getCode() { return code; }
+    public String getCode() { return code; }
     public String getMessage() { return message; }
 }
