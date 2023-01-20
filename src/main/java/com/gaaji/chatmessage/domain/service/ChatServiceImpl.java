b455de256302bf6e4f9956a32aa4b8214c905b6c
Seungho <hsh47607@naver.com>
2023-01-20 16:29:02 +0900

    refactor: GM-116 비동기 처리
    - Kafka 메시지 발행 및 Chat Db 저장 등 메시지 브로드캐스팅 이외의 작업에 대하여 비동기 처리를 진행
    - 비동기 처리를 위해 ThreadPool을 생성하였음.
    - ThreadPool은 총 3개가 생성
    - WebSocketConnectServiceImpl kafkaConnectThreadPool
    - ChatServiceImpl kafkaConnectThreadPool
    - ChatServiceImpl databaseConnectThreadPool
    - ThreadPool의 Thread는 IntegerConstants에서 컴파일시 정의되는 현재 시스템 CPU의 최대 가용 코어 수(n)만큼 생성된다.
    - 따라서 총 생성되는 Thread는 최대 n * 3이다.
    
    close GM-116

--- src/main/java/com/gaaji/chatmessage/domain/service/ChatServiceImpl.java
+++ src/main/java/com/gaaji/chatmessage/domain/service/ChatServiceImpl.java
@@ -3,35 +3,37 @@ package com.gaaji.chatmessage.domain.service;
 import com.gaaji.chatmessage.domain.controller.dto.ChatDto;
 import com.gaaji.chatmessage.domain.entity.Chat;
 import com.gaaji.chatmessage.domain.repository.ChatRepository;
-import com.gaaji.chatmessage.global.constants.ApiConstants;
+import com.gaaji.chatmessage.global.constants.IntegerConstants;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
 public class ChatServiceImpl implements ChatService{
 
-    private final SimpMessageSendingOperations template;
     private final ChatRepository chatRepository;
     private final KafkaService kafkaService;
+    private final ExecutorService kafkaConnectThreadPool =
+            Executors.newFixedThreadPool(IntegerConstants.RUNTIME_CORE_COUNT,
+                    new ThreadFactoryBuilder().setNameFormat("Gaaji-Chat-Kafka-Connect-Thread-%d").build());
+
+    private final ExecutorService databaseConnectThreadPool =
+            Executors.newFixedThreadPool(IntegerConstants.RUNTIME_CORE_COUNT,
+                    new ThreadFactoryBuilder().setNameFormat("Gaaji-Chat-Database-Connect-Thread-%d").build());
 
     @Override
     public void chat(ChatDto chatDto) {
-        Chat chat = Chat.from(chatDto);
+        // Kafka 메시지 발행
+        kafkaConnectThreadPool.submit(() -> kafkaService.chat(chatDto));
 
-        // 1. DB 저장
-        chatRepository.save(chat);
-
-        // 2. 구독자에게 브로드캐스트
-        broadcasting(chatDto);
-
-        // 3. API 서버 메시지 전달
-        kafkaService.chat(chatDto);
-
-    }
-
-    private void broadcasting(ChatDto chat) {
-        template.convertAndSend(ApiConstants.SUBSCRIBE_ENDPOINT + chat.getRoomId(), chat);
+        // DB 저장
+        databaseConnectThreadPool.submit(() -> {
+            Chat chat = Chat.from(chatDto);
+            chatRepository.save(chat);
+        });
     }
 }
--- src/main/java/com/gaaji/chatmessage/domain/service/WebSocketConnectServiceImpl.java
+++ src/main/java/com/gaaji/chatmessage/domain/service/WebSocketConnectServiceImpl.java
@@ -2,8 +2,13 @@ package com.gaaji.chatmessage.domain.service;
 
 import com.gaaji.chatmessage.domain.entity.Session;
 import com.gaaji.chatmessage.domain.repository.SessionRepository;
+import com.gaaji.chatmessage.global.constants.IntegerConstants;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Component
 @RequiredArgsConstructor
@@ -12,13 +17,18 @@ public class WebSocketConnectServiceImpl implements WebSocketConnectService {
     private final SessionRepository sessionRepository;
     private final KafkaService kafkaService;
 
+    private final ExecutorService kafkaConnectThreadPool =
+            Executors.newFixedThreadPool(IntegerConstants.RUNTIME_CORE_COUNT,
+                    new ThreadFactoryBuilder().setNameFormat("Gaaji-Connect-Kafka-Connect-Thread-%d").build());
+
+
     @Override
     public void connect(String sessionId, String userId) {
         Session connectSession = Session.of(sessionId, userId);
-
         sessionRepository.save(connectSession);
 
-        kafkaService.notifyOnline(userId);
+        kafkaConnectThreadPool.submit(() ->
+                kafkaService.notifyOnline(connectSession.getUserId()));
     }
 
     @Override
@@ -26,8 +36,10 @@ public class WebSocketConnectServiceImpl implements WebSocketConnectService {
         sessionRepository.findBySessionId(sessionId)
                 .ifPresent(
                         session -> {
-                            kafkaService.notifyOffline(session.getUserId());
                             sessionRepository.delete(session);
+
+                            kafkaConnectThreadPool.submit(() ->
+                                    kafkaService.notifyOffline(session.getUserId()));
                         }
                 );
     }
