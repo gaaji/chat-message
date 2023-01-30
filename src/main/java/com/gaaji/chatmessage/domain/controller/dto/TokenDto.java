bc528997bbeb55c1601fca19d677c2a0e10537d0
Seungho <hsh47607@naver.com>
2023-01-30 16:25:40 +0900

    refactor: 필요없는 라인 삭제

--- src/main/java/com/gaaji/chatmessage/domain/controller/dto/TokenDto.java
+++ src/main/java/com/gaaji/chatmessage/domain/controller/dto/TokenDto.java
@@ -8,8 +8,4 @@ import lombok.*;
 public class TokenDto {
     private final String tokenHeaderName = StringConstants.HEADER_SOCKET_TOKEN;
     private final String token;
-
-//    public static TokenDto of(String token) {
-//        return new TokenDto(StringConstants.HEADER_SOCKET_TOKEN, token);
-//    }
 }
